a308f73cea1b33144b05a0094f8582384e7716f8
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2025-02-09 11:19:52 +0200

    edits

--- apps/shared/core/lib/views/pages/splash_screen.dart
+++ apps/shared/core/lib/views/pages/splash_screen.dart
@@ -1,13 +1,11 @@
 import 'dart:async';
 import 'package:core/core.dart';
+import 'package:core/views/pages/car_painter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({
-    super.key,
-    required this.carColor,
-  });
+  const SplashScreen({super.key, required this.carColor});
 
   final Color carColor;
 
@@ -40,9 +38,7 @@ class _SplashScreenState extends State<SplashScreen>
   Future<void> moveToNextScreen() async {
     Timer(
       const Duration(seconds: 6),
-      () async {
-        await context.navigator.pushNamed(AppRoutes.onBoardingRoute);
-      },
+      () async => await context.navigator.pushNamed(AppRoutes.onBoardingRoute),
     );
   }
 
@@ -58,57 +54,15 @@ class _SplashScreenState extends State<SplashScreen>
       body: Center(
         child: AnimatedBuilder(
           animation: _controller,
-          builder: (context, child) {
-            return Transform.translate(
-              offset: Offset(_slideAnimation.value, 0),
-              child: CustomPaint(
-                size: Size(200.w, 100.h),
-                painter: CarPainter(
-                  carColor: widget.carColor,
-                ),
-              ),
-            );
-          },
+          builder: (context, child) => Transform.translate(
+            offset: Offset(_slideAnimation.value, 0),
+            child: CustomPaint(
+              size: Size(200.w, 100.h),
+              painter: CarPainter(carColor: widget.carColor),
+            ),
+          ),
         ),
       ),
     );
   }
-}
-
-class CarPainter extends CustomPainter {
-  final Color carColor;
-
-  const CarPainter({required this.carColor});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = carColor
-      ..style = PaintingStyle.fill;
-
-    final path = Path()
-      ..moveTo(0, size.height * 0.6)
-      ..lineTo(size.width * 0.2, size.height * 0.6)
-      ..lineTo(size.width * 0.3, size.height * 0.4)
-      ..lineTo(size.width * 0.7, size.height * 0.4)
-      ..lineTo(size.width * 0.8, size.height * 0.6)
-      ..lineTo(size.width, size.height * 0.6)
-      ..lineTo(size.width, size.height * 0.8)
-      ..lineTo(0, size.height * 0.8)
-      ..close();
-
-    final wheelPaint = Paint()
-      ..color = Colors.black
-      ..style = PaintingStyle.fill;
-
-    final leftWheelCenter = Offset(size.width * 0.25, size.height * 0.8);
-    final rightWheelCenter = Offset(size.width * 0.75, size.height * 0.8);
-
-    canvas.drawPath(path, paint);
-    canvas.drawCircle(leftWheelCenter, size.height * 0.1, wheelPaint);
-    canvas.drawCircle(rightWheelCenter, size.height * 0.1, wheelPaint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
